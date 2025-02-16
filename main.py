0c55664e7e33a5ed6649db679b6a35a04b87bb38
Cathal Lawlor <cathal.lawlor@outlook.com>
2025-02-16 12:13:04 +0000

    added rudimentary gridsearch

--- main.py
+++ main.py
@@ -138,13 +138,13 @@ def genetic_algorithm(pop_size=100, generations=2000, crossover_rate=0.8, mutati
             print(f'Stopping early at generation {generation} due to no improvement in fitness.')
             break
 
-    best_tour = population[genome_fitness.index(min(genome_fitness))]
+    best_ga_tour = population[genome_fitness.index(min(genome_fitness))]
     best_loop_distance = min(genome_fitness)
-    best_loop_distance2 = total_distance(best_tour)
+    best_loop_distance2 = total_distance(best_ga_tour)
     print("Best loop distance:{}, best 2 loop {}", best_loop_distance, best_loop_distance2)
     plot_fitness_over_time(best_fitness_over_time)
-    plot_tour(best_tour, nodes, best_loop_distance)
-    return best_tour, best_loop_distance
+    # plot_tour(best_ga_tour, nodes, best_loop_distance)
+    return best_ga_tour, best_loop_distance
 
 def plot_fitness_over_time(fitness_scores):
     plt.plot(fitness_scores)
@@ -153,6 +153,39 @@ def plot_fitness_over_time(fitness_scores):
     plt.title("Fitness over Time")
     plt.show()
 
+# Gridsearch for the best possible hyperparameters
+# MUTATION_RATES = [0.01, 0.05, 0.12]
+# CROSSOVER_RATES = [0.7, 0.8, 0.9]
+# POPULATION_SIZES = [100, 200, 350]
+MUTATION_RATES = [0.01,]
+CROSSOVER_RATES = [0.8]
+POPULATION_SIZES = [350]
+
+def gridsearch():
+    best_params = None
+    best_ga_distance = float('inf')
+
+    # Gridsearch for the best possible combination of hyperparameters
+    # Hyperparameters: Population size, Mutation rate, Crossover rate
+    for POPULATION_SIZE in POPULATION_SIZES:
+        for MUTATION_RATE in MUTATION_RATES:
+            for CROSSOVER_RATE in CROSSOVER_RATES:
+                print(
+                    f'Running GA with Population size: {POPULATION_SIZE}, Mutation rate: {MUTATION_RATE}, Crossover rate: {CROSSOVER_RATE}')
+                current_tour, current_distance = (
+                    genetic_algorithm(pop_size=POPULATION_SIZE, generations=400, crossover_rate=CROSSOVER_RATE, mutation_rate=MUTATION_RATE))
+                if current_distance < best_ga_distance:
+                    plot_tour(nodes, current_tour, current_distance)
+                    best_solution, best_ga_distance = current_tour, current_distance
+                    best_params = (POPULATION_SIZE, MUTATION_RATE, CROSSOVER_RATE)
+                print('----------------------------------------------')
+
+    print('Best Params: ', best_params)
+    print("Best Tour:", best_tour)
+    print("Best Distance:", best_ga_distance)
+    return
+
+
 if __name__ == "__main__":
     filename = "berlin52.txt"
 
@@ -160,8 +193,9 @@ if __name__ == "__main__":
     nodes, distance_matrix = parse_tsplib(filename)
     start_time = time.time()
 
-    best_tour, best_distance = genetic_algorithm()
+    gridsearch()
+    # best_tour, best_distance = genetic_algorithm()
     end_time = time.time()
     print("Computation time taken:", end_time - start_time, "seconds")
-    print("Best Tour:", best_tour)
-    print("Best Distance:", best_distance)
+    # print("Best Tour:", best_tour)
+    # print("Best Distance:", best_distance)
